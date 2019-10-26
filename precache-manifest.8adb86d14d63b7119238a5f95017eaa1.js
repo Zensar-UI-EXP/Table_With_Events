2b55bb66c39cf498468c295620c0874622b2fbbc
Mohamed <mohamed.p@zensar.com>
2019-10-26 22:42:39 +0530

    Pushing build files

---
+++ precache-manifest.8adb86d14d63b7119238a5f95017eaa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8dfb95e53cd618a8658834ed3255f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3857a585f3363d1753bc",
+    "url": "/static/css/main.7415552a.chunk.css"
+  },
+  {
+    "revision": "93cd55f45a7a88bb648d",
+    "url": "/static/js/2.e27cf4de.chunk.js"
+  },
+  {
+    "revision": "3857a585f3363d1753bc",
+    "url": "/static/js/main.39d26bc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
